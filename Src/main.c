3f1b0d91c40aa9baf208e51eecdebdc38bd2fe9d
Jacob <thejacobcohen@gmail.com>
2025-09-20 20:47:34 -0400

    Added back mainloop counter

--- Src/main.c
+++ Src/main.c
@@ -46,17 +46,16 @@ int main(void)
     i2c_nvic_config();                                  // I2C interrupt configuration
     input_init();                                       // Input initialization
 
-    delay_1ms(500);                                     // give time for ros to startup
+    //delay_1ms(500);                                     // give time for ros to startup
 
     while(1) {
 
-        delay_1ms(DELAY_IN_MAIN_LOOP);
-
-        handle_mpu6050();                               // Handle of the MPU-6050 IMU sensor
-        //handle_sensors();                               // Handle of the optical sensors
-        handle_usart();                                 // Handle of the USART data
-        //handle_leds();                                  // Handle of the sideboard LEDs
-
+      delay_1ms(DELAY_IN_MAIN_LOOP);
+      handle_mpu6050();                               // Handle of the MPU-6050 IMU sensor
+      //handle_sensors();                               // Handle of the optical sensors
+      handle_usart();                                 // Handle of the USART data
+      // handle_leds();                                  // Handle of the sideboard LEDs
+      main_loop_counter++;
     }
 }
 
